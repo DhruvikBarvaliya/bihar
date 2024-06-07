707dc73a557fdd6ad36bbeb20a7d543356be9a7d
DhruvikBarvaliya <dhruvik.barvaliya.blackwolve@gmail.com>
2024-06-07 12:20:39 +0530

    change in swagger doc

--- swagger.js
+++ swagger.js
@@ -1,26 +1,30 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 // Basic metadata about the API
 const swaggerDefinition = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'Material Requisition System API',
-    version: '1.0.0',
-    description: 'API documentation for the Material Requisition System',
+    title: "Material Requisition System API",
+    version: "1.0.0",
+    description: "API documentation for the Material Requisition System",
   },
   servers: [
     {
-      url: 'http://localhost:3000',
-      description: 'Development server',
+      url: "http://localhost:3000/api/v1",
+      description: "Development server",
+    },
+    {
+      url: "https://bihar-diop.onrender.com/api/v1",
+      description: "Production server",
     },
   ],
   components: {
     securitySchemes: {
       BearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
       },
     },
   },
@@ -33,11 +37,11 @@ const swaggerDefinition = {
 
 const options = {
   swaggerDefinition,
-  apis: ['./routes/*.js'], // Path to the API docs
+  apis: ["./routes/*.js"], // Path to the API docs
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
 module.exports = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
